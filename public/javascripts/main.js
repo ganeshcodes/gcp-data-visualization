ef4cd4e8fa352d49a38e38ba9833d2db7855030e
Ganeshbabu Thavasimuthu <gxt.gatsby@Ganeshbabus-MacBook-Pro.local>
2018-03-07 11:49:53 -0600

    cnstring

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.countrypiebtn').click(function(){
+    /*$('.countrypiebtn').click(function(){
         $.ajax({url: "/countrypie", success: function(resp){
             data = [['CountryCode', 'Count']];
             for(var i in resp){
@@ -9,7 +9,30 @@ $(document).ready(function(){
             console.log('%o',data);
             drawChart(data);
         }});
+    });*/
+
+    $("#inpform").submit(function(e) {
+        var url = "/countrypie"; // the script where you handle the form input.
+        $.ajax({
+               type: "POST",
+               url: url,
+               data: $("#inpform").serialize(), // serializes the form's elements.
+               success: function(resp)
+               {
+                    console.log('%o',resp);
+                    data = [['CountryCode', 'Count']];
+                    for(var i in resp){
+                        row = resp[i];
+                        data.push([row.CountryCode, parseInt(row.count)])
+                    }
+                    console.log('%o',data);
+                    drawChart(data);
+               }
+             });
+    
+        e.preventDefault(); // avoid to execute the actual submit of the form.
     });
+
 });
 
 function drawChart(data) {
